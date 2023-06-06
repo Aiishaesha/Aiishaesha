db4e2d28e289fcdc32bb5035237c6079f3c2bb10
Aiishaesha <135683264+Aiishaesha@users.noreply.github.com>
2023-06-06 10:19:16 +0600

    Update cod.cpp(3 vers)

--- cod.cpp
+++ cod.cpp
@@ -1,11 +1,27 @@
-int a, b=0, c=1;
-cout<<"Введите число Фибоначчи: "<<endl;
-cin>>a; 
-for(int i=2;i<=(a-3);i++)
-{
-        a=b+c;
-        b=c;
-        c=a+b;
-}
-cout<<"Число Фибоначчи равно = "<<c<<endl;
+#include <iostream>
 
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+
+    int prev = 0;
+    int curr = 1;
+
+    for (int i = 2; i <= n; i++) {
+        int temp = curr;
+        curr = prev + curr;
+        prev = temp;
+    }
+
+    return curr;
+}
+
+int main() {
+    int n;
+    std::cout << "Enter the value of n: ";
+    std::cin >> n;
+
+    std::cout << "The " << n << "th Fibonacci number is: " << fibonacci(n) << std::endl;
+
+    return 0;
+}
