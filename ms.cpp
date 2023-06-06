88b1d4ed0001a50bb203ea73616a4863ec98cacb
Aiishaesha <135683264+Aiishaesha@users.noreply.github.com>
2023-06-06 10:11:42 +0600

    Update ms.cpp(1 vers)

--- ms.cpp
+++ ms.cpp
@@ -1,1 +1,28 @@
-nmnnnmn,m  m 
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if (n==1)
+	{
+		cout << 0;
+	}else if (n == 2)
+	{
+		cout << 1;
+	}
+	else if (n>2)
+	{
+		int a;
+		vector<int> g{0,1};
+		for (size_t i = 2; i < n; i++)
+		{
+			a = g[i - 1] + g[i - 2];
+			g.push_back(a);
+			a = 0;
+		}
+		cout << g[n-1];
+	}
+	
+}
